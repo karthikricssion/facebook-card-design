2746e6b597a89c8b448dfa19bd1ea878f377bcbb
karthikeyan rajendran <karthik_ricssion@yahoo.com>
2020-05-18 11:07:21 +0530

    Worked on implementing video post option

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,29 @@
 window.onload = function() {
+    var isFullScreen = function() {
+        return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+    }
+
+    var setFullscreenData = function(state) {
+        fs.setAttribute('data-fullscreen', !!state);
+    }
+
+    var handleFullscreen = function() {
+        if (isFullScreen()) {
+            if (document.exitFullscreen) document.exitFullscreen();
+            else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+            else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+            else if (document.msExitFullscreen) document.msExitFullscreen();
+            setFullscreenData(false);
+         }
+         else {
+            if (videoContainer.requestFullscreen) videoContainer.requestFullscreen();
+            else if (videoContainer.mozRequestFullScreen) videoContainer.mozRequestFullScreen();
+            else if (videoContainer.webkitRequestFullScreen) videoContainer.webkitRequestFullScreen();
+            else if (videoContainer.msRequestFullscreen) videoContainer.msRequestFullscreen();
+            setFullscreenData(true);
+         }
+    }
+
     var changeButtonState = function(type) {
         
         if (type == 'playpause') {
@@ -23,5 +48,26 @@ window.onload = function() {
     playpause.addEventListener('click', function(e) {
         if (video.paused || video.ended) video.play();
         else video.pause();
+    });
+
+    fs.addEventListener('click', function(e) {
+        handleFullscreen();
+    });
+
+
+    document.addEventListener('fullscreenchange', function(e) {
+        setFullscreenData(!!(document.fullScreen || document.fullscreenElement));
+    });
+
+    document.addEventListener('webkitfullscreenchange', function() {
+        setFullscreenData(!!document.webkitIsFullScreen);
+    });
+
+    document.addEventListener('mozfullscreenchange', function() {
+        setFullscreenData(!!document.mozFullScreen);
+    });
+    
+    document.addEventListener('msfullscreenchange', function() {
+        setFullscreenData(!!document.msFullscreenElement);
     });
 }
